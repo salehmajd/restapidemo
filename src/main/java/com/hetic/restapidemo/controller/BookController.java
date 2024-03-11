fbeb2cc5c34ec6025653546944d80ce516350963
Majd Saleh <majd.sa@gmail.com>
2024-03-11 16:35:22 +0100

    add create book endpoint

--- src/main/java/com/hetic/restapidemo/controller/BookController.java
+++ src/main/java/com/hetic/restapidemo/controller/BookController.java
@@ -3,9 +3,7 @@ package com.hetic.restapidemo.controller;
 import com.hetic.restapidemo.dto.Book;
 import com.hetic.restapidemo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +13,11 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    @PostMapping("")
+    public Book create(@RequestBody Book book) {
+        return bookService.save(book);
+    }
 
     @GetMapping("")
     public List<Book> findAll() {
